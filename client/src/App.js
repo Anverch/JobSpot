db66a064d8f8fe3f43c48dfd047bb22bbd5a42cd
Emily Wirtz <ewirtz3@gmail.com>
2020-08-01 13:24:49 -0500

    semantic

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
 
 function App() {
   return (
