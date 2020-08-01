e1168c983cba9a1a4a2e6f27737f8c51a575a2f2
Emily Carlisle <emilydeluna96@gmail.com>
2020-08-01 18:35:17 -0500

    link to signup page

--- client/src/components/Pages/Signin/index.js
+++ client/src/components/Pages/Signin/index.js
@@ -1,35 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {
-    Button,
-    Form,
-    Grid,
-    Header,
-    Message,
-    Segment
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
 } from "semantic-ui-react";
 import "./style.css";
 
 export default function SignIn() {
-    return (
-        <Grid className="styleGrid">
-            <div className="body" />
-            <div className="grad" />
-            <Grid.Column className="colStyle">
-                <Header className="header" as="h2">
-                    <div>
-                        Job<span>Spot</span>
-                    </div>
-                </Header>
-                <Form className="login">
-                    <Segment stacked>
-                        <Form.Input fluid icon="user" iconPosition="left" placeholder="E-mail Address" type="text" />
-                        <Form.Input fluid icon="lock" iconPosition="left" placeholder="Password" type="password" />
-                    </Segment>
-                    <Button className="button" value="Login">Login</Button>
-                        
-                </Form>
-            </Grid.Column>
-        </Grid>
-    )
+  return (
+    <Grid className="styleGrid">
+      <div className="body" />
+      <div className="grad" />
+      <Grid.Column className="colStyle">
+        <Header className="header" as="h2">
+          <div>
+            Job<span>Spot</span>
+          </div>
+        </Header>
+        <Form className="login">
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="E-mail Address"
+              type="text"
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
+          </Segment>
+          <Button className="button" value="Login">
+            Login
+          </Button>
+          <Message className="signup">
+            New to us?{" "}
+            <Link
+              to="/signin"
+              className={
+                window.location.pathname === "/signin"
+                  ? "nav-link active"
+                  : "nav=link"
+              }
+            >
+              Sign Up
+            </Link>
+          </Message>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  );
 }
