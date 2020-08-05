79030b9c2e96ed10fde2a0399d47f616436d891f
Mackenzie Schutz <mshoots64@gmail.com>
2020-08-05 14:01:57 -0500

    add job update route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,24 +2,40 @@ const db = require("../models")
 
 module.exports = {
     getJobs(req, res) {
-        db.Job 
-        .findAll()
-        .then(dbJob => res.json(dbJob))
-        .catch(err => res.status(500).json(err));
+        db.Job
+            .findAll()
+            .then(dbJob => res.json(dbJob))
+            .catch(err => res.status(500).json(err));
+    },
+    createJob(req, res) {
+        db.Job
+            .create(req.body)
+            .then(dbJob => res.json(dbJob))
+            .catch(err => res.status(500).json(err));
     },
     getJob(req, params) {
         const id = req.params.id;
         db.Job
-        .findOne({
-            where: { id: id }
-        })
-        .then(dbJob => res.json(dbJob))
-        .catch(err=> res.status(500).json(err));
+            .findOne({
+                where: { id: id }
+            })
+            .then(dbJob => res.json(dbJob))
+            .catch(err => res.status(500).json(err));
     },
-    createJob(req, res) {
-        db.Job 
-        .create(req.body)
-        .then(dbJob => res.json(dbJob))
-        .catch(err => res.status(500).json(err));
+    updateJob(req, params) {
+        const id = req.params.id;
+        db.Job
+            .update({
+                company: req.body.text,
+                job_title: req.body.text,
+                salary: req.body.text,
+                status: req.body.text
+            }, {
+                where: {
+                    id: id
+                }
+            }).then(function (dbJob) {
+                res.json(dbJob)
+            })
     }
 }
