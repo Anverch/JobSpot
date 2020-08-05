5f863fd5192c4cab9a200662d6d175d8bdc1e5e0
Mackenzie Schutz <mshoots64@gmail.com>
2020-08-04 20:15:06 -0500

    save file structure

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
 const Router = require('express').Router;
+const userRoutes = require('./users');
 
 const apiRoutes = Router();
 
+apiRoutes.use('/users', userRoutes);
 
 module.exports = apiRoutes;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,9 @@
 const Router = require('express').Router;
 const apiRoutes = require('./apiRoutes');
-const htmlRoutes = require('./htmlRoutes');
 
 const router = new Router();
 
 router.use('/api', apiRoutes);
-router.use('', htmlRoutes);
-
 
 
 module.exports = router;
