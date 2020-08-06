11409e11104dee256e74c31627eed16aea45f0a8
Mackenzie Schutz <mshoots64@gmail.com>
2020-08-06 08:45:08 -0500

    update api routes for users

--- routes/users/index.js
+++ routes/users/index.js
@@ -3,5 +3,12 @@ const usersController = require("../../controllers/user");
 
 // api/jobs
 router.route("/")
-.get(usersController.getUsers);
-.post(jobsController.createJob);
+.get(usersController.getUsers)
+.post(jobsController.createUser);
+
+router.route("/:id") 
+.get(usersController.getUser)
+.put(usersController.updateUser)
+.delete(usersController.deleteUser);
+
+module.exports = router
