e7fb5faa58153a92ea7035cb7f5420c2e7194d26
Aleksandr Saiyan <anverch@gmail.com>
2020-08-05 22:46:30 -0500

    update models

--- models/jobs.js
+++ models/jobs.js
@@ -1,18 +1,28 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-    const Job = sequelize.define(
-        'Job',
+    const Jobs = sequelize.define(
+        'jobs',
         {
             id: {
                 type: DataTypes.INTEGER,
                 autoIncrement: true,
                 primaryKey: true
-            }, 
+            },
+            
             company: DataTypes.STRING,
             job_title: DataTypes.STRING,
             salary: DataTypes.INTEGER
-            // status: Interested, applied,  ect. (drop down)
         }
-    )
-    return Job;
+    );
+    Jobs.associate = function (models) {
+        // associations can be defined here
+        Jobs.belongsTo(models.User, {
+            onDelete: "cascade",
+            foreignKey: {
+                name: 'user_id',
+                allowNull: false
+            }
+        });
+    }
+    return Jobs;
 };
