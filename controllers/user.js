1de07624cd19db9e007f354bde18fdb94196a6f5
Mackenzie Schutz <mshoots64@gmail.com>
2020-08-06 08:40:40 -0500

    add update user controller

--- controllers/user.js
+++ controllers/user.js
@@ -21,5 +21,21 @@ module.exports = {
             .create(req.body)
             .then(dbUser => res.json(dbUser))
             .catch(err => res.status(500).json(err));
-    }
+    },
+    updateUser (req, params) {
+        const id = req.params.id;
+        db.User
+            .update({
+                name: req.body.text,
+                email: req.body.text,
+                password: req.body.text
+            }, {
+                where: {
+                    id: id
+                }
+            }).then(function (dbUser) {
+                res.json(dbUser)
+            })
+    },
+    
 }
