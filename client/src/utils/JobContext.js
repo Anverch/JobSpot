d5cb9ed782304e1fae10f325181fcc2c7bde08f9
Emily Wirtz <ewirtz3@gmail.com>
2020-08-10 20:08:10 -0500

    things

---
+++ client/src/utils/JobContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useContext } from "react";
+export const JobModel = {
+  companyName: "",
+  positionTitle: "",
+  yearlySalary: 0,
+  currentStatus: "interested",
+  phoneInterview: new Date(),
+  inPersonInterview: new Date(),
+  fullBenefits: "",
+  location: "",
+  jobNotes: "",
+};
+
+const JobContext = createContext(JobModel);
+
+export function JobProvider({ value, children }) {
+  return <JobContext.Provider value={value}>{children}</JobContext.Provider>;
+}
+
+export function useJobContext() {
+  return useContext(JobContext);
+}
