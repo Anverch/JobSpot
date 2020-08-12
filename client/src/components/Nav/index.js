1b9aff659d7d55a3bfdd39dd454760e0a7d3fdc0
Mackenzie Schutz <mshoots64@gmail.com>
2020-08-11 20:08:28 -0500

    nav bar updated

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,24 +1,36 @@
 import React from "react";
-import { Header, Grid } from "semantic-ui-react";
-import MenuDropdown from "../MenuDropdown";
+import { Header, Grid, Menu } from "semantic-ui-react";
 import "./Nav.css";
 
 export default function Nav() {
   return (
     <>
       <Grid>
-        <Grid.Row id="nav" columns={2} verticalAlign="middle">
+        <Grid.Row id="nav" columns={1} verticalAlign="middle">
           <Grid.Column textAlign="center">
             <Header as="h2" id="nav-logo">
               <span id="job">Job</span>
               Spot
             </Header>
           </Grid.Column>
-          <Grid.Column textAlign="center">
-            <MenuDropdown />
-          </Grid.Column>
         </Grid.Row>
       </Grid>
+      <div class="ui secondary pointing menu">
+        <a class="item active">
+          Dashboard
+  </a>
+        <a class="item">
+          Add New Job
+  </a>
+        <div class="right menu">
+          <a class="ui item">
+            Logout
+    </a>
+        </div>
+      </div>
+      <div class="ui segment">
+        <p></p>
+      </div>
     </>
   );
 }
