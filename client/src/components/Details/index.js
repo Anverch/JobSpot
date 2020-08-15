d01d9e740f157090ecf28945a917c16876c8719a
Emily Wirtz <ewirtz3@gmail.com>
2020-08-15 11:17:59 -0500

    job view working

--- client/src/components/Details/index.js
+++ client/src/components/Details/index.js
@@ -3,7 +3,7 @@
 //use reducer to filter
 
 //importing dependencies
-import React from "react";
+import React, { useEffect } from "react";
 import { Header, Form, Grid, TextArea } from "semantic-ui-react";
 import UpdateButton from "../UpdateButton";
 import { useUserContext } from "../../utils/GlobalState";
@@ -11,19 +11,21 @@ import { useUserContext } from "../../utils/GlobalState";
 export default function Details() {
   // invokes useUserContext(),
   const [state, dispatch] = useUserContext();
-  console.log(`state:>>`, state);
+  useEffect(() => {
+    console.log(`state.activeJob:>>`, state.activeJob), {};
+  });
 
   return (
     <>
       <Grid rows={5}>
         <Grid.Row columns={2} id="meta-row">
           <Grid.Column textAlign="left">
-            <Header as="h3">Facebook</Header>
-            <span>Backend Developer</span>
+            <Header as="h3">{state.activeJob.companyName}</Header>
+            <span>{state.activeJob.positionTitle}</span>
           </Grid.Column>
           <Grid.Column textAlign="right">
-            <Header as="h4">$95,000</Header>
-            <Header sub>Current status: In Process</Header>
+            <Header as="h4">{state.activeJob.salary}</Header>
+            <Header sub>Current status: {state.activeJob.currentStatus}</Header>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row columns={3} id="details1-row">
@@ -39,10 +41,10 @@ export default function Details() {
         </Grid.Row>
         <Grid.Row id="details2-row" columns={3}>
           <Grid.Column>
-            <Header sub>Full benefits: yes</Header>
+            <Header sub>Full benefits: {state.activeJob.fullBenefits}</Header>
           </Grid.Column>
           <Grid.Column>
-            <Header sub>Location: Remote</Header>
+            <Header sub>Location: {state.activeJob.location}</Header>
           </Grid.Column>
           <Grid.Column>
             <Header sub></Header>
