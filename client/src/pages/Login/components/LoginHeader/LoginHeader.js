5bbf1774df51cf15cd1b1a94868d6ee08c366294
Emily Carlisle <emilydeluna96@gmail.com>
2020-08-18 16:19:26 -0500

    loginheader component

---
+++ client/src/pages/Login/components/LoginHeader/LoginHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+import "./styles.css";
+
+const LoginHeader = () => (
+    <Header>
+        <div>
+            Job<span>Spot</span>
+        </div>
+    </Header>
+)
+export default LoginHeader;
