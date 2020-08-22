1d64ab75d78385cf1c2089718f332ff58bb8434d
Emily Carlisle <emilydeluna96@gmail.com>
2020-08-21 23:01:51 -0500

    navbar styles

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,6 +4,27 @@ import { Link, useHistory } from "react-router-dom";
 import "./Nav.css";
 import API from "../../utils/API";
 
+const styles = {
+  navContainer: {
+    backgroundColor: "rgb(22 25 29)"
+  },
+  navHeader: {
+    textAlign: "center",
+    fontSize: "4.5em",
+    color: "white",
+    fontFamily: "Times New Roman"
+  },
+  spanHeader: {
+    color: "#F25C05"
+  },
+  navItems: {
+    color: "white",
+    textDecoration: "none",
+    fontSize: 18,
+    fontFamily: "Times New Roman"
+  }
+}
+
 export default function Nav() {
   const history = useHistory();
   const handleItemClick = (e) => {
@@ -14,23 +35,23 @@ export default function Nav() {
 
   return (
     <>
-      <Container id="nav-container">
-        <Header as="h2" id="nav-logo">
-          <span id="job">Job</span>Spot
+      <Container fluid style={styles.navContainer} id="nav-container">
+        <Header style={styles.navHeader} as="h2" id="nav-logo">
+          <span style={styles.spanHeader} id="job">Job</span>Spot
         </Header>
-        <Menu id="nav-menu">
+        <Menu secondary pointing id="nav-menu">
           <Menu.Item
             id="dashboard-item"
             name="dashboard"
           >
-            <Link to="/home" id="nav-link-home">Dashboard</Link>
+            <Link style={styles.navItems} to="/home" id="nav-link-home">Dashboard</Link>
           </Menu.Item>
           <Menu.Item id="job-item" name="Add New Job">
-            <Link to= "/create-job" id="nav-link-create">Add New Job</Link>
+            <Link style={styles.navItems} to= "/create-job" id="nav-link-create">Add New Job</Link>
           </Menu.Item>
-          <Menu.Menu id="nav-menu2">
+          <Menu.Menu position="right" id="nav-menu2">
             <Menu.Item id="logout-btn" name="Logout" onClick={handleItemClick}>
-              <Link id="nav-link-logout" onClick={handleItemClick}>
+              <Link style={styles.navItems} id="nav-link-logout" onClick={handleItemClick}>
                 Logout
               </Link>
             </Menu.Item>
