cb0103923c99a1a200e830364938ad6cb1ed7303
Emily Carlisle <emilydeluna96@gmail.com>
2020-08-22 03:26:50 -0500

    footer styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,11 +2,35 @@ import React from "react";
 import { Container, List } from "semantic-ui-react";
 import "./Footer.css";
 
+const styles = {
+  footerContainer: {
+    backgroundColor: "rgb(22 25 29)",
+    textAlign: "center",
+    padding: 10,
+    fontFamily: "Times New Roman",
+    width: "100%",
+    display: "inline-block",
+    fontSize: "1em",
+    margin: "0px!important",
+    color: "#F29F05",
+    position: "fixed",
+    left: 0,
+    bottom: 0,
+  
+  },
+  github: {
+    color: "#F24405",
+    fontSize: "1.3em",
+    fontWeight: "bold",
+    textDecoration: "underline",
+    fontFamily: "Times New Roman"
+  }
+}
 export default function Footer() {
   return (
-    <Container id="footer-container">
+    <Container fluid style={styles.footerContainer} id="footer-container">
       <List id="footer-list">
-        <List.Item id="github"
+        <List.Item style={styles.github} id="github"
           as="a"
           href="https://github.com/anverch/jobspot"
           target="_blank"
@@ -18,7 +42,7 @@ export default function Footer() {
         Copyright 2020 Emily Wirtz, Aleks Saiyan, Emily Carlisle, & Mackenzie
         Schutz
         </p>
-
     </Container>
+
   );
 }
