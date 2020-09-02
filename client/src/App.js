76914e35ed17c52ec092f008db71f8081bc5d172
Emily Wirtz <ewirtz3@gmail.com>
2020-09-02 13:22:08 -0500

    removing useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
